56bda689d4be00b5318e82848e96aa7f9cf02028
Gustavo Lira <gu.lirar23@gmail.com>
2022-09-07 15:08:13 -0500

    add some example of basic classes and objects

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    int x = 5;
+    public static void main(String[] args){
+        Main myObj = new Main();
+        Main myObj2 = new Main();
+        System.out.println(myObj.x);
+        System.out.println(myObj2.x);
+    }
+}
