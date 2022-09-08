4b6eee127075f2436355f285285e9bf3e1c5af3c
Gustavo Lira <gu.lirar23@gmail.com>
2022-09-08 12:12:41 -0500

    add and instance of person and use get and set methods

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,14 @@ public class Main {
         Main myObj2 = new Main();
         System.out.println(myObj.x);
         System.out.println(myObj2.x);
+
+        Person person1 = new Person();
+        Person person2 = new Person();
+        person1.setName("John");
+        person2.setName("Peter");
+
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+
     }
 }
