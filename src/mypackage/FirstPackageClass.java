c3a3a5c6c6d387e9b872f69fe31f4244daefd6c4
Gustavo Lira <gu.lirar23@gmail.com>
2022-09-08 13:05:45 -0500

    create a package and a package class

---
+++ src/mypackage/FirstPackageClass.java
@@ -0,0 +1,14 @@
+package mypackage;
+
+public class FirstPackageClass {
+    int x;
+    int y;
+
+    public FirstPackageClass(int value1, int value2){
+        x = value1;
+        y = value2;
+    }
+    public int sum(){
+        return x + y;
+    }
+}
