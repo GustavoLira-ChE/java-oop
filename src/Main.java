a553cb35a8d9976cda2dcc744afdc52d4b6a13c4
Gustavo Lira <gu.lirar23@gmail.com>
2022-09-08 16:38:17 -0500

    add an example of inheritance and polymorphism

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import mypackage.FirstPackageClass;
+import animalPackage.*;
 public class Main {
     int x = 5;
     public static void main(String[] args){
@@ -15,15 +16,26 @@ public class Main {
         System.out.println(person1.getName());
         System.out.println(person2.getName());
 
+        //Package
         FirstPackageClass myObj3 = new FirstPackageClass(4,5);
         int result = myObj3.sum();
         System.out.println(result);
 
+        //Inheritance
         Car myCar = new Car("VW","Polo");
         Car myCar2 = new Car("Mazda","3");
         System.out.println(myCar.getModel());
         System.out.println(myCar2.getModel());
         myCar.honk();
+
+        //Inheritance and polymorphism
+        Animal myAnimal = new Animal();
+        Animal myDog = new Dog();
+        Animal myPig = new Pig();
+
+        myAnimal.animalSound();
+        myDog.animalSound();
+        myPig.animalSound();
 
     }
 }
