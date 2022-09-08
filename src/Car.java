b2785cfa1f9d19b517da1359cd6a4dd0b611699a
Gustavo Lira <gu.lirar23@gmail.com>
2022-09-08 16:28:53 -0500

    add a subclass Car

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Car {
+class Car extends Vehicle{
+    private String model;
+    public Car(String brand, String model) {
+        super(brand);
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
 }
