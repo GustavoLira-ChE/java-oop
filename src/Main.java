8b9286302bf01a3bfae04d41538e5aa911cba5ff
Gustavo Lira <gu.lirar23@gmail.com>
2022-09-08 13:06:17 -0500

    add an example to import a user-defined package class

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import mypackage.FirstPackageClass;
 public class Main {
     int x = 5;
     public static void main(String[] args){
@@ -13,6 +14,10 @@ public class Main {
 
         System.out.println(person1.getName());
         System.out.println(person2.getName());
+
+        FirstPackageClass myObj3 = new FirstPackageClass(4,5);
+        int result = myObj3.sum();
+        System.out.println(result);
 
     }
 }
