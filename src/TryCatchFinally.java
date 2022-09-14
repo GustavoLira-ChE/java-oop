4eefdf5d5080110595ea55154e86e338f34ff41a
Gustavo Lira <gu.lirar23@gmail.com>
2022-09-14 10:54:48 -0500

    add an example of try, catch, finally statement

--- src/TryCatchFinally.java
+++ src/TryCatchFinally.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class TryCatchFinally {
+public class TryCatchFinally {
+    public static void main(String[] args){
+        try{
+            int[] numbers = {1,2,3,4,5};
+            System.out.println(numbers[10]);
+        }catch(Exception e){
+            System.out.println("Try statement throw an error: " + e);
+        }finally{
+            System.out.println("The 'try..catch' statement finished.");
+        }
+    }
 }
